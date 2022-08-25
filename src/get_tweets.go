8b9351eb39cc81849282b19880c926ef430eae5d
h-kato0610 <h.kato0610@gmail.com>
2022-08-26 08:55:51 +0900

    update divided function

--- src/get_tweets.go
+++ src/get_tweets.go
@@ -8,23 +8,30 @@ import (
 )
 
 func main() {
-        bearerToken := "" // Input your Bearer Twitter Token
-        client := gotwtr.New(bearerToken)
+	bearerToken := "" // Input your Bearer Twitter Token
+	client := gotwtr.New(bearerToken)
 
-        searchTweet := "ぽよ〜"
-        tsr, err := client.SearchRecentTweets(context.Background(), searchTweet, &gotwtr.SearchTweetsOption{
-            TweetFields: []gotwtr.TweetField{
-                gotwtr.TweetFieldAuthorID,
-                gotwtr.TweetFieldAttachments,
-            },
-            MaxResults: 10,
-        })
-        if err != nil {
-            log.Fatal(err)
-        }
+	searchWord := "ぽよ〜"
+	tsr := getSearchPubTweets(client, searchWord)
 
-        for _, t := range tsr.Tweets {
-            log.Println("----------------------------------------")
-            log.Println(t.Text)
-        }
+	for _, t := range tsr.Tweets {
+		log.Println("----------------------------------------")
+		log.Println(t.Text)
+	}
+}
+
+func getSearchPubTweets(client *gotwtr.Client, searchWord string) (tsr *gotwtr.SearchTweetsResponse) {
+	maxResult := 10
+	tsr, err := client.SearchRecentTweets(context.Background(), searchWord, &gotwtr.SearchTweetsOption{
+		TweetFields: []gotwtr.TweetField{
+			gotwtr.TweetFieldAuthorID,
+			gotwtr.TweetFieldAttachments,
+		},
+		MaxResults: maxResult,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return tsr
 }
